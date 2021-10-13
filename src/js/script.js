a3978bef0a814cd074d6cdc5d109ba5ab0b8ec23
Maxim Ivannikov <84341083+MaximIVannikov@users.noreply.github.com>
2021-10-13 20:18:29 +0200

    tiny-slider added2

--- src/js/script.js
+++ src/js/script.js
@@ -23,19 +23,7 @@ var slider = tns({
     autoplay: false,
     controls: false,
     nav: false,
-    responsive: {
-        640: {
-          edgePadding: 20,
-          gutter: 20,
-          items: 2
-        },
-        700: {
-          gutter: 30
-        },
-        900: {
-          items: 3
-        }
-      }
+    
   });
 
   document.querySelector('.prev').addEventListener('click', function () {
