3b9190d6334a43d9849fdc5277bf40b5e88ec5bf
Maxim Ivannikov <84341083+MaximIVannikov@users.noreply.github.com>
2021-10-17 09:46:59 +0200

    validate form

--- src/js/script.js
+++ src/js/script.js
@@ -48,6 +48,42 @@ toggleSlide('.catalog-item__back');
       $('.overlay, #order').fadeIn('slow');
     })
   });
+
+ // validation form
+
+  function validateForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          
+            required: true,
+            minlength: 2
+          
+        },
+        phone: "required",
+        email: {
+          required: true,
+          email: true
+        }
+      },
+        messages: {
+          name: {
+            required: "Пожалуйста, введите свое имя",
+            minlength: jQuery.validator.format("Введите {0} символа!")
+          },
+          phone: "Пожалуйста, введите свой номер телефона",
+          email: {
+            required: "Пожалуйста, введите свой почтовый адрес",
+            email: "Неправильно введен адрес почты"
+          }
+      }
+    });
+  };
+
+  validateForms('#consultation-form');
+  validateForms('#order form');
+  validateForms('#consultation form');
+
 });
 
 
