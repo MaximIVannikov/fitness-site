481e380d05aa0142a68e7838d0650ef57ea3b832
Maxim Ivannikov <84341083+MaximIVannikov@users.noreply.github.com>
2021-10-17 18:42:43 +0200

    after validate

--- src/js/script.js
+++ src/js/script.js
@@ -117,7 +117,7 @@ toggleSlide('.catalog-item__back');
     }
   });
 
-  $("a[href^='#']").click(function(){
+  $("a[href=#up]").click(function(){
     const _href = $(this).attr("href");
     $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
     return false;
